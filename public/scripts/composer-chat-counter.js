774f0326acea1a0814be35e2df3e56a1d96f4d2e
nolangendron <nolanglengendron@gmail.com>
2019-06-11 21:24:51 +0000

    Add a function to event handler to count number of characters in tweet form textarea, add add a class to change counter to red when negative numbers

---
+++ public/scripts/composer-chat-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    const maxTweetLength = 140;
+  $('#tweet-text').on('keyup', function() {
+    let counter = $(this).siblings('.counter');
+    let lengthCounter = $(this).val().length;
+    lengthCounter = maxTweetLength - lengthCounter;
+    $(counter).text(lengthCounter);
+    if (lengthCounter < 0) {
+      $(counter).addClass('counterRed');
+    } else {
+      $(counter).removeClass('counterRed');
+    }
+    })
+  });
+
+
+
+
